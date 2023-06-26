2298dd62c0c638a1f8086065679b52d6f781d4be
soyedarif <soyedariff@gmail.com>
2023-06-27 01:10:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from './components/Home'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <Home/>
+    </>
+  )
+}
+
+export default App
